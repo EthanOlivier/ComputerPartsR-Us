664b30c56b9ba147fb3587ecaab78ca792492d27
EthanOlivier <122174496+EthanOlivier@users.noreply.github.com>
2024-07-01 15:59:46 -0400

    Add project files.

---
+++ GHLikeGame/Main.Designer.cs
@@ -0,0 +1,75 @@
+﻿using WMPLib;
+namespace GHLikeGame
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.Background = new System.Windows.Forms.PictureBox();
+            this.Song = new WindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.Background)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Background
+            // 
+            this.Background.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Background.Image = ((System.Drawing.Image)(resources.GetObject("Background.Image")));
+            this.Background.Location = new System.Drawing.Point(0, 0);
+            this.Background.Name = "Background";
+            this.Background.Size = new System.Drawing.Size(800, 450);
+            this.Background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Background.TabIndex = 0;
+            this.Background.TabStop = false;
+            this.Background.SendToBack();
+            // 
+            // Song
+            // 
+            this.Song = new WindowsMediaPlayer();
+            this.Song.URL = "C:\\Users\\Ethan\\source\\repos\\GHLikeGame\\GHLikeGame\\tomp3.cc - Goukisan  Betrayal of Fear.wav";
+            this.Song.controls.play();
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Background);
+            this.Name = "Main";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.Background)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Background;
+        private WindowsMediaPlayer Song;
+    }
+}
+
---
+++ GHLikeGame/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GHLikeGame
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
